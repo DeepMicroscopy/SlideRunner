15286767d54b17dcfef74149072d0542c7b83bbd
Marc Aubreville <marc.aubreville@fau.de>
2018-02-06 18:21:16 +0100

    Added tests for main database functions

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='SlideRunner',
+      version='1.7.0',
+      description='SlideRunner - A Tool for Massive Cell Annotations in Whole Slide Images',
+      url='http://github.com/maubreville/SlideRunner',
+      author='Marc Aubreville',
+      author_email='marc.aubreville@fau.de',
+      license='GPL',
+      packages=['SlideRunner'],
+      install_requires=[
+          'PyQt5', 'openslide-python>=1.1.1', 'pyqt5>=5.5.0', 'opencv-python>=3.1.0',
+          'matplotlib>=2.0.0', 'numpy>=1.13', 'matplotlib>=2.0.0'
+      ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    zip_safe=False)
---
+++ tests/test_database.py
@@ -0,0 +1,70 @@
+from SlideRunner.dataAccess.database import *
+import os
+
+def test_database():
+    DB = Database()
+    assert(DB is not None)
+    assert(DB.isOpen() == False)
+    
+    dbname = 'sample_test_database.db'
+    DB.create(dbname)
+
+    assert(DB.isOpen() == True)
+    assert(DB.getDBname() == dbname)
+    
+    DB.insertAnnotator('John Doe')
+    DB.insertAnnotator('Jane Doe')
+    pers = DB.getAllPersons()
+    assert((pers[0][0]=='John Doe'))
+    assert((pers[0][1]==1))
+    assert((pers[1][0]=='Jane Doe'))
+    assert((pers[1][1]==2))
+
+    DB.insertClass('Cell')
+    DB.insertClass('Crap')
+
+    classes = DB.getAllClasses()
+    assert((classes[0][0]=='Cell'))
+    assert((classes[0][1]==1))
+    assert((classes[1][0]=='Crap'))
+    assert((classes[1][1]==2))
+
+    DB.insertNewSlide('BLA.tiff')
+
+    assert(DB.findSlideWithFilename('BLA.tiff')==1)
+
+    DB.insertNewSpotAnnotation(xpos_orig=5,ypos_orig=5, slideUID=1, classID=2, annotator=1, type = 1)
+    DB.insertNewSpotAnnotation(xpos_orig=5,ypos_orig=15, slideUID=1, classID=2, annotator=1, type = 1)
+    DB.insertNewSpotAnnotation(xpos_orig=5,ypos_orig=8, slideUID=1, classID=2, annotator=2, type = 1)
+    DB.insertNewSpotAnnotation(xpos_orig=5,ypos_orig=4, slideUID=1, classID=1, annotator=2, type = 1)
+
+    spotannos = DB.findSpotAnnotations(leftUpper=(0,0), rightLower=(10,10), slideUID=1, blinded = False, currentAnnotator=1)
+    assert(spotannos[0][0]==5) # x coordinate
+    assert(spotannos[0][1]==5) # y coordinate
+    assert(spotannos[0][2]==2) # agreed Class
+
+    assert(len(spotannos)==3) # entry 2 is not found
+
+    # test blinded mode
+    spotannos = DB.findSpotAnnotations(leftUpper=(0,0), rightLower=(10,10), slideUID=1, blinded = True, currentAnnotator=2)
+    assert(spotannos[0][0]==5) # x coordinate
+    assert(spotannos[0][1]==5) # y coordinate
+    assert(spotannos[0][2]==0) # agreed Class is unknown, because blinded
+
+    # test statistics
+    (names,stats) = DB.countEntryPerClass()
+    
+    assert(names[0]=='Cell')
+    assert(names[1]=='Crap')
+
+    assert(stats[1,0]==1)
+    assert(stats[1,1]==3)
+
+
+    cleanup()
+    
+    pass
+
+
+def cleanup():
+    os.remove('sample_test_database.db')
