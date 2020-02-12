43566a9a324701302c051f7ee2a912f9ed5e3841
Marc Aubreville <marc.aubreville@fau.de>
2020-02-12 08:09:52 +0100

    Improved DB manager to include buttons and allow removing EXACT link from multiple items.

--- SlideRunner/gui/dialogs/dbmanager.py
+++ SlideRunner/gui/dialogs/dbmanager.py
@@ -45,16 +45,22 @@ class DatabaseManager(QDialog):
             self.tableWidget.setItem(row,2, QTableWidgetItem(str(fileToAnnos[int(idx)]) if (int(idx)) in fileToAnnos else '0'))
             self.tableWidget.setItem(row,3, QTableWidgetItem(str(exactid)))
 
+            btn = QPushButton('open')
+            self.tableWidget.setCellWidget(row,4, btn)
+
+            btn.clicked.connect(partial(self.loadFile, self.los[row]))
+
+
     def createTable(self):
        # Create table
         self.tableWidget = QTableWidget()
-        self.tableWidget.setColumnCount(4)
-        self.tableWidget.setHorizontalHeaderLabels(['ID','Filename','Annotations', 'EXACT Image ID'])
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setHorizontalHeaderLabels(['ID','Filename','Annotations', 'EXACT Image ID', ''])
         self.updateTable()
         self.tableWidget.move(0,0)
         self.tableWidget.resizeColumnsToContents()
         self.tableWidget.viewport().installEventFilter(self)
-        
+        self.tableWidget.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
     def loadFile(self, uid):
         
         slidepath=(uid[-1]+os.sep+uid[1]) if os.path.exists((uid[-1]+os.sep+uid[1])) else uid[1]
@@ -64,14 +70,7 @@ class DatabaseManager(QDialog):
                 self.close()
         else:
             QtWidgets.QMessageBox.information(self,'Not found',f'The file {slidepath} could not be found.')
-#        reply = QtWidgets.QMessageBox.question(self, 'Question',
-#                                        'Do you really want to delete the file and all annotations from the database?', QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
 
-#        if reply == QtWidgets.QMessageBox.No:
-#            return
-#        self.DB.removeFileFromDatabase(uid[0])
-#        self.updateTable()
-#        self.show()
 
     def removeFile(self, uid):
         reply = QtWidgets.QMessageBox.question(self, 'Question',
@@ -84,13 +83,18 @@ class DatabaseManager(QDialog):
         self.updateTable()
         self.show()
 
-    def removeExactLink(self, uid):
-        reply = QtWidgets.QMessageBox.question(self, 'Question',
-                                        'Do you really want to remove the link to EXACT for this slide?', QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+    def removeExactLink(self, uids:list):
+        if (len(uids)>1):
+            reply = QtWidgets.QMessageBox.question(self, 'Question',
+                                            'Do you really want to remove the link to EXACT for these slides?', QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+        else:
+            reply = QtWidgets.QMessageBox.question(self, 'Question',
+                                            'Do you really want to remove the link to EXACT for this slide?', QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
 
         if reply == QtWidgets.QMessageBox.No:
             return
-        self.DB.execute(f'UPDATE Slides SET exactImageID=Null where uid=={uid[0]}')
+        for uid in uids:
+            self.DB.execute(f'UPDATE Slides SET exactImageID=Null where uid=={uid}')
         self.DB.commit()
         self.updateTable()
         self.show()
@@ -99,12 +103,17 @@ class DatabaseManager(QDialog):
         if(event.type() == QtCore.QEvent.MouseButtonPress and
            event.buttons() == QtCore.Qt.RightButton and
            source is self.tableWidget.viewport()):
+            rows = np.unique([x.row() for x in self.tableWidget.selectedItems()])
+            rowuids = [self.los[x][0] for x in rows]
             item = self.tableWidget.itemAt(event.pos())
             if item is not None:
                 menu = QMenu(self)
                 menu.addAction('Open '+self.los[item.row()][1], partial(self.loadFile, self.los[item.row()]))         #(QAction('test'))
                 menu.addAction('Remove '+self.los[item.row()][1], partial(self.removeFile, self.los[item.row()]))         #(QAction('test'))
-                menu.addAction('Remove EXACT link for '+self.los[item.row()][1], partial(self.removeExactLink, self.los[item.row()]))         #(QAction('test'))
+                if (len(rows)>1):
+                    menu.addAction('Remove EXACT link selected rows ', partial(self.removeExactLink, rowuids))         #(QAction('test'))
+                else:
+                    menu.addAction('Remove EXACT link for '+self.los[item.row()][1], partial(self.removeExactLink, rowuids))         #(QAction('test'))
                 menu.exec_(event.globalPos())
         return super(DatabaseManager, self).eventFilter(source, event)
 
